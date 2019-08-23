dae35584e1c93bd0c5ab0b7ee083484de037183b
Andrielle Calegari <andrielle.calegari@hotmail.com>
2019-08-23 19:23:10 -0300

    Versão Final

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,35 +2,8 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
-@browser = ENV['BROWSER']
-
-if @browser.eql?('headless')
-  Capybara.javascript_driver = :selenium
-  Capybara.run_server = false
-
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'chromeOption' => {
-      'args' => ['--no-default-browser-check']
-    }
-  )
-
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(
-      app, 
-      browser: :remote,
-      url: 'http://selenium_server:4444/wd/hub',
-      desired_capabilities: caps
-    )
-  end
-end
-
 Capybara.configure do |c|
-  if @browser.eql?('headless')
-    c.default_driver = :selenium
-  elseif @browser.eql?('chrome')
-    c.default_driver = :selenium_chrome
-  end
-
+  c.default_driver = :selenium_chrome
   c.app_host = 'http://ninjamotors.herokuapp.com'
   c.default_max_wait_time = 10
 end
